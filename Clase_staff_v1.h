c4efd7c99eb520d9e25fa4669ac99ddfd27b0ade
Alien300owo <a01425472@tec.mx>
2023-11-25 12:05:20 -0600

    Proyecto_POO_v1
    Se crearon los archivos que se usarán y se puso lo básico para trabajar, falta realizar la lista para el sistema de seguridad

---
+++ Clase_staff_v1.h
@@ -0,0 +1,16 @@
+#import <iostream>
+#import <string>
+
+using namespace std;
+
+class staff{
+	private: // Atributos
+		string nombre;
+		double salario;
+		
+				
+	//public: // Metodos
+	//Nombre--------
+	
+};
+
