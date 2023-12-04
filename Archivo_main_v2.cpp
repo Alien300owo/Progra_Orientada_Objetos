5afde35cdee60bd7d3265c8068917d6cb9b18539
Alien300owo <a01425472@tec.mx>
2023-12-03 22:09:17 -0600

    Programa Completo
    Se agrego todo lo mínimo para que el proyecto sea

--- Archivo_main_v2.cpp
+++ Archivo_main_v2.cpp
@@ -1,3 +1,5 @@
+// Paolo Martini, Carlos Olamendi
+
 // Archivo main para el programa
 // En este codigo esta:
 //	1. Ciclo de seguridad
@@ -18,7 +20,7 @@ char opcion;
 
 // codigo main	
 int main(){
-	/***
+
 	string contra="";
 	string user="";
 	//Arreglos (Listas del sistema de seguridad que tiene a los usurios y contrasenas)
@@ -50,44 +52,49 @@ int main(){
 			}
 		}
 	}
-	***/
 	
-	int cantidad_empleados = 3;
-	staff perfil_empleado[cantidad_empleados];
+	int cantidad_empleados = 50;
+	staff perfil_empleado[cantidad_empleados]; //lista objeto
+	int id = 0;
 	//Menu de la aplicacion (menu en el que muestra las distintas opciones y si el usuario ingresa alguna,)
 	//						(se realiza la accion que pide el usuario)
 	do{
+		//Seleccion de opcione en el menu
 	    cout << "a)Agregar nuevo \nb)Consultar \nc)Salarios y Bonos \nd)Modificar \ne)Salir \nOpcion: ";
 	    cin >> opcion;
-	
+		
 	    switch (opcion){
-		    case 'a':{
+		    //Permite ingresar nuevos empleados 
+			case 'a':{
 		    	for (int i = 0; i < cantidad_empleados; ++i){
-				staff empleado;
+				staff empleado; //objeto
+				cout << "Empleado id: " << id << endl;
 				empleado.datos_empleado();
 				perfil_empleado[i] = empleado;
+				id = id + 1;
 				}
 		        break;
 		    }
-		
+			// Permite consultar los datos del empleado
 		    case 'b':{
-		    	int empleado_id = 0;
-		    	cout << "Empleado id: " << endl;
-		    	cin >> empleado_id;
-		    	perfil_empleado[empleado_id].Consultar();
-		        break;
-		    }
-		
-		    case 'c':{
-		    	int empleado_id = 0;
-		    	cout << "Empleado id: " << endl;
-		    	cin >> empleado_id;
-		    	perfil_empleado[empleado_id].salarios_y_bonos();
+		    	cout << "Empleado id: ";
+		    	cin >> id;
+		    	perfil_empleado[id].Consultar();
 		        break;
 		    }
 		    
+			// Permite aumentar el salario del empleado por los bonos
+		    case 'c':{
+		    	cout << "Empleado id: " << endl;
+		    	cin >> id;
+		    	perfil_empleado[id].salarios_y_bonos();
+		        break;
+		    }
+		    // Permite cambiar la informacion de un empleado pero no cambia de posicion
 		    case 'd':{
-		    	
+		    	cout << "Empleado id" << endl;
+		    	cin >> id;
+		    	perfil_empleado[id].datos_empleado();
 		        break;
 		    }
 		
--- Clase_staff_v1.h
+++ Clase_staff_v1.h
@@ -7,32 +7,32 @@ class staff{
 	private: // Atributos
 		string nombre;
 		double salario;
-		int id;
 		
 				
 	public: // Metodos
-	//Nombre--------
+	//Nombre y salario ingresar
 	void datos_empleado(){
-		cout << "Nombre: ";
+		
+		cout << "Nombre nuevo: ";
 		cin >> nombre;
 		
 		cout << "salario inicial: $";
 		cin >> salario;
-		
-		cout << "id: ";
-		cin >> id;
 	}
 	
+	//permite ver los datos del usuario
 	void Consultar(){
 		cout << "Nombre: " << nombre << endl;
 		cout << "salario inicial: $" << salario << endl;
 		//cout << "id: " << id << endl;
 	}
 	
+	// Permite agregar dinero a los bonos gracias a los bonos
 	void salarios_y_bonos(){
 		int horas_extra = 0;
+		cout << "Nombre: " << nombre << endl;
 		cout << "salario inicial: $" << salario << endl;
-		cout << "Horas extras: " <<;
+		cout << "Horas extras: ";
 		cin >> horas_extra;
 		salario = salario + (horas_extra * 200);
 		cout << "nuevo salario: $" << salario << endl;
